6c72e9591ec5703a88cbf19e77704cec1ff7e22e
vss-2 <32624576+vss-2@users.noreply.github.com>
2018-10-21 19:39:23 -0300

    Adicionado primeiro DEC
    Corrigidas também tabulações, comentários e etc

--- Controle.sv
+++ Controle.sv
@@ -1,6 +1,6 @@
 INC_1:begin
-    		muxA = 3; //Mux A recebe 3, que é o valor do registrador estendido paga 32.
-		muxB = 4; //Mux B recebe 4, que é 1.
+    		muxA = 3; 		//Mux A recebe 3, que é o valor do registrador estendido paga 32.
+		muxB = 4; 		//Mux B recebe 4, que é 1.
 		muxALU = 0;
 		muxPC = 0;
 		muxZero = 0;
@@ -12,9 +12,9 @@ INC_1:begin
 		muxMfhi = 0;
 		muxMflo = 0;
 		WR = 0;
-		loadRegALU = 1;
-		loadRegA = 1;
-		loadRegB = 1;
+		loadRegALU = 1;		//Resultado armazenado na ULA
+		loadRegA = 0;
+		loadRegB = 0;
 		loadRegMemData = 0;
 		loadRegEPC = 0;
 		loadRegMfhi = 0;
@@ -25,8 +25,8 @@ INC_1:begin
 		escrevePC = 0;
 		escrevePCcond = 0;
 		case(opcode)
-			6'h8: controleALU = 3'b001;//add
-			6'h9: controleALU = 3'b001;//add
+			6'h8: controleALU = 3'b001;	//Adicao
+			6'h9: controleALU = 3'b001;	//Adicao
 			default: controleALU = 3'b111;
 		endcase
 		Shift_regDes = 3'b000;
@@ -61,17 +61,17 @@ INC_2:begin
 		escrevePC = 0;
 		escrevePCcond = 0;
 		case(opcode)
-			6'h8: controleALU = 3'b001;//add
-			6'h9: controleALU = 3'b001;//add
-			default: controleALU = 3'b111;//
+			6'h8: controleALU = 3'b001;	//Ambos fazem adicao
+			6'h9: controleALU = 3'b001;
+			default: controleALU = 3'b111;
 		endcase
 		Shift_regDes = 3'b000;
 		regBanco = 0;
 		if(overflow == 1'b1)
 		begin
 			case(opcode)
-				6'h9: prox_estado = ARIT_IMM_3;  //addiu
-				default: prox_estado = EXCES_1;	 //EXCESSAO de overflow
+				6'h9: prox_estado = ARIT_IMM_3;  //Addiu 
+				default: prox_estado = EXCES_1;	 //Excecao de overflow
 			endcase
 		end
 		else	
@@ -85,8 +85,8 @@ INC_3:begin
 		muxALU = 0;
 		muxPC = 0;
 		muxZero = 0;
-		muxRegEscr = 0; //Esse Mux será usado para gravar no registrador
-		muxDadEscr = 0; //Esse Mux será usado para gravar o dado
+		muxRegEscr = 0; 	//Esse Mux será usado para gravar no registrador
+		muxDadEscr = 0; 	//Esse Mux será usado para gravar o dado
 		muxMemData = 0;
 		muxN = 0;
 		muxRs = 0;
@@ -106,11 +106,48 @@ INC_3:begin
 		escrevePC = 0;
 		escrevePCcond = 0;
 		case(opcode)
-			6'h8: controleALU = 3'b001;    //add
-			6'h9: controleALU = 3'b001;    //add
+			6'h8: controleALU = 3'b001;    	//Ambos fazem add
+			6'h9: controleALU = 3'b001;    	
 			default: controleALU = 3'b111;
 		endcase
 		Shift_regDes = 3'b000;
 		regBanco = 1;
 		prox_estado = INICIO;
 end
+
+DEC_1:begin
+    		muxA = 3; 		//Mux A recebe 3, que é o valor do registrador estendido paga 32.
+		muxB = 4; 		//Mux B recebe 4, que é 1.
+		muxALU = 0;
+		muxPC = 0;
+		muxZero = 0;
+		muxRegEscr = 0;
+		muxDadEscr = 0;
+		muxMemData = 0;
+		muxN = 0;
+		muxRs = 0;
+		muxMfhi = 0;
+		muxMflo = 0;
+		WR = 0;
+		loadRegALU = 1;		//Resultado armazenado na ULA
+		loadRegA = 0;
+		loadRegB = 0;
+		loadRegMemData = 0;
+		loadRegEPC = 0;
+		loadRegMfhi = 0;
+		loadRegMflo = 0;
+		iniciarMult = 0;
+		iniciarDiv = 0;
+		escreverIR = 0;
+		escrevePC = 0;
+		escrevePCcond = 0;
+		case(opcode)
+			6'h8: controleALU = 3'b010;    //Sub
+			6'h9: controleALU = 3'b010;    //Sub
+			default: controleALU = 3'b111;
+		endcase
+		Shift_regDes = 3'b000;
+		regBanco = 0;
+		prox_estado = DEC_2;
+end
+

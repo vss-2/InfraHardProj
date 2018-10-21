f16741f258c8307ab84e1d8397c2556397cad8a1
vss-2 <32624576+vss-2@users.noreply.github.com>
2018-10-21 19:51:02 -0300

    Correções de próximo estado
    E inclusão do DEC_3

--- Controle.sv
+++ Controle.sv
@@ -70,13 +70,12 @@ INC_2:begin
 		if(overflow == 1'b1)
 		begin
 			case(opcode)
-				6'h9: prox_estado = ARIT_IMM_3;  //Addiu 
+				6'h9: prox_estado = INC_3;  //Addiu 
 				default: prox_estado = EXCES_1;	 //Excecao de overflow
 			endcase
 		end
 		else	
-			prox_estado = ARIT_IMM_3;
-		prox_estado = INC_2;
+		prox_estado = INC_3;
 end
 
 INC_3:begin
@@ -187,11 +186,46 @@ DEC_2:begin
 		if(overflow == 1'b1)
 		begin
 			case(opcode)
-				6'h9: prox_estado = ARIT_IMM_3;  //Subi 
+				6'h9: prox_estado = DEC_3;  //Subi 
 				default: prox_estado = EXCES_1;	 //Excecao de overflow
 			endcase
 		end
 		else	
-			prox_estado = ARIT_IMM_3;
-		prox_estado = DEC_2;
+		prox_estado = DEC_3;
+end
+
+DEC_3:begin
+		muxA = 3;
+		muxB = 4;
+		muxALU = 0;
+		muxPC = 0;
+		muxZero = 0;
+		muxRegEscr = 0; 	//Esse Mux será usado para gravar no registrador
+		muxDadEscr = 0; 	//Esse Mux será usado para gravar o dado
+		muxMemData = 0;
+		muxN = 0;
+		muxRs = 0;
+		muxMfhi = 0;
+		muxMflo = 0;
+		WR = 0;
+		loadRegALU = 1;
+		loadRegA = 0;
+		loadRegB = 0;
+		loadRegMemData = 0;
+		loadRegEPC = 0;
+		loadRegMfhi = 0;
+		loadRegMflo = 0;
+		iniciarMult = 0;
+		iniciarDiv = 0;
+		escreverIR = 0;
+		escrevePC = 0;
+		escrevePCcond = 0;
+		case(opcode)
+			6'h8: controleALU = 3'b010;    	//Ambos fazem sub
+			6'h9: controleALU = 3'b010;    	
+			default: controleALU = 3'b111;
+		endcase
+		Shift_regDes = 3'b000;
+		regBanco = 1;
+		prox_estado = INICIO;
 end

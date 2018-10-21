c757429f61a7875f88cc99b38387fda0eb9deeb3
vss-2 <32624576+vss-2@users.noreply.github.com>
2018-10-21 19:44:56 -0300

    Segundo estágio de DEC

--- Controle.sv
+++ Controle.sv
@@ -151,3 +151,47 @@ DEC_1:begin
 		prox_estado = DEC_2;
 end
 
+DEC_2:begin
+		muxA = 3;
+		muxB = 4;
+		muxALU = 0;
+		muxPC = 0;
+		muxZero = 0;
+		muxRegEscr = 0; 
+		muxDadEscr = 0; 
+		muxMemData = 0;
+		muxN = 0;
+		muxRs = 0;
+		muxMfhi = 0;
+		muxMflo = 0;
+		WR = 0;
+		loadRegALU = 1;
+		loadRegA = 0;
+		loadRegB = 0;
+		loadRegMemData = 0;
+		loadRegEPC = 0;
+		loadRegMfhi = 0;
+		loadRegMflo = 0;
+		iniciarMult = 0;
+		iniciarDiv = 0;
+		escreverIR = 0;
+		escrevePC = 0;
+		escrevePCcond = 0;
+		case(opcode)
+			6'h8: controleALU = 3'b010;	//Ambos fazem subtracao
+			6'h9: controleALU = 3'b010;
+			default: controleALU = 3'b111;   
+		endcase
+		Shift_regDes = 3'b000;
+		regBanco = 0;
+		if(overflow == 1'b1)
+		begin
+			case(opcode)
+				6'h9: prox_estado = ARIT_IMM_3;  //Subi 
+				default: prox_estado = EXCES_1;	 //Excecao de overflow
+			endcase
+		end
+		else	
+			prox_estado = ARIT_IMM_3;
+		prox_estado = DEC_2;
+end

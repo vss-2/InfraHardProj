6261d1865f3bc3c10005a3846c3955d88ef79a46
vss-2 <32624576+vss-2@users.noreply.github.com>
2018-10-22 22:07:36 -0300

    Criação SinalExtendido

---
+++ SinalExtendido.sv
@@ -0,0 +1,10 @@
+//Recebe como entrada endereço de tamanho 16, e devolve saida tamanho 32
+//existem 2 possibilidades, ser negativo ou positivo.
+module SinalExtendido (input logic [15:0] Address, output logic [31:0] AddresExt);
+	always_comb begin
+		if(Address[15] == 1'b1)
+			AddresExt = 32'b11111111111111110000000000000000 + Address; //Caso negativo
+		else
+			AddresExt = 32'b00000000000000000000000000000000 + Address; //Caso positivo
+	end
+endmodule: SinalExtendido
